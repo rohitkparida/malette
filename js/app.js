883d157641b3fe9d297b97830e0fa02b3ccfc629
Rohit Parida <rohitkparida@gmail.com>
2018-10-29 16:31:35 +0530

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,23 +1,12 @@
-// var person = {
-//     firstName:"John",
-//     lastName:"Doe",
-//     age:46};
-//
-//
-// document.getElementById("demo").innerHTML = person["firstName"];
-
-
-//anchor-for-scrolling-------------------------------------
-
-// $('a[href*="#"]:not([href="#"])').click(function() {
-//     if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-//         var target = $(this.hash);
-//         target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-//         if (target.length) {
-//             $('html, body').animate({
-//                 scrollTop: target.offset().top
-//             }, 700);
-//             return false;
-//         }
-//     }
-// });
+$.ajax({
+    url: "data.js",
+    dataType: "json",
+    type: "get",
+    cache: false,
+    success: function(data) {
+        $(data.apps).each(function(i, item) {
+            // $('ul#grid-wrapper').append("<li class='card " + data.apps[i].appType + "'> <div class = 'app-img-wrapper'> <img style = 'background:" + data.apps[i].imgcolor + "' class ='app-img' src = '" + data.apps[i].imgsrc + "'> </img> </div> <div class = 'title-bar'> <a href = '#' class = 'card-title'>" + data.apps[i].title + " </a> <a href = '" + data.apps[i].link + "'target = '_blank' rel = 'noopener'> <img class = 'open-btn'src = 'assets/open.svg'alt = 'open-svg'/> </a> </div> </li>");
+            console.log(item.title[i]);
+        });
+    }
+});
