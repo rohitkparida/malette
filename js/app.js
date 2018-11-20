20ece6799162a6377c393b119924a9c050427965
Rohit Parida <rohitkparida@gmail.com>
2018-11-20 18:05:24 +0530

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,3 +1,16 @@
+// $.ajax({
+//     url: "palette.json",
+//     dataType: "json",
+//     type: "get",
+//     cache: false,
+//     success: function(data) {
+//         $(data.colors).each(function(i, item) {
+//             $('div.content').append('');
+//         });
+//     }
+// });
+//
+
 $.ajax({
     url: "palette.json",
     dataType: "json",
@@ -5,27 +18,30 @@ $.ajax({
     cache: false,
     success: function(data) {
         $(data.colors).each(function(i, item) {
-            // <div class="color-vessel">
-            //     <div class="color-title" id="red">Red</div>
-            //     <div class="color red-50"><span>50</span><span class="right">#FFEBEE</span></div>
-            //     <div class="color red-100"><span>100</span><span class="right">#FFCCD2</span></div>
-            //     <div class="color red-200"><span>200</span><span class="right">#EF9A9A</span></div>
-            //     <div class="color red-300"><span>300</span><span class="right">#E57373</span></div>
-            //     <div class="color red-400 white-text"><span>400</span><span class="right">#EF5350</span></div>
-            //     <div class="color red-500 white-text"><span>500</span><span class="right">#F44336</span></div>
-            //     <div class="color red-600 white-text"><span>600</span><span class="right">#E53935</span></div>
-            //     <div class="color red-700 white-text"><span>700</span><span class="right">#D32F2F</span></div>
-            //     <div class="color red-800 white-text"><span>800</span><span class="right">#C62828</span></div>
-            //     <div class="color red-900 white-text"><span>900</span><span class="right">#B71C1C</span></div>
-            //     <div class="color-title" id="red">Red Accents</div>
-            //     <div class="color red-a100"><span>A100</span><span class="right">#1a237E</span></div>
-            //     <div class="color red-a200 white-text"><span>A200</span><span class="right">#1a237E</span></div>
-            //     <div class="color red-a400 white-text"><span>A400</span><span class="right">#1a237E</span></div>
-            //     <div class="color red-a700 white-text"><span>A700</span><span class="right">#1a237E</span></div>
-            // </div>
-            $('div.content').append('<div class="color-vessel"> <div class="color-title" id="' + data.colors[i].color + '">' + data.colors[i].color + '</div><div class="color" style="background-color:' + data.colors[i].fifty + '"><span>50</span><span style="float: right">' + data.colors[i].fifty + '</span></div><div class="color" style="background-color:' + data.colors[i].hundred + '"><span>100</span><span style="float: right">' + data.colors[i].hundred + '</span></div><div class="color" style="background-color:' + data.colors[i].twohundred + '"><span>200</span><span style="float: right">' + data.colors[i].twohundred + '</span></div><div class="color" style="background-color:' + data.colors[i].threehundred + '"><span>300</span><span style="float: right">' + data.colors[i].threehundred + '</span></div><div class="color" style="background-color:' + data.colors[i].fourhundred + '"><span>400</span><span style="float: right">' + data.colors[i].fourhundred + '</span></div><div class="color" style="background-color:' + data.colors[i].fivehundred + '"><span>500</span><span style="float: right">' + data.colors[i].fivehundred + '</span></div><div class="color" style="background-color:' + data.colors[i].sixhundred + '"><span>600</span><span style="float: right">' + data.colors[i].sixhundred + '</span></div><div class="color" style="background-color:' + data.colors[i].sevenhundred + '"><span>700</span><span style="float: right">' + data.colors[i].sevenhundred + '</span></div><div class="color" style="background-color:' + data.colors[i].eighthundred + '"><span>800</span><span style="float: right">' + data.colors[i].eighthundred + '</span></div><div class="color" style="background-color:' + data.colors[i].ninehundred + '"><span>900</span><span style="float: right">' + data.colors[i].ninehundred + '</span></div><div class="color-title" id="' + data.colors[i].color + '">' + data.colors[i].color + ' Accents</div><div class="color" style="background-color:' + data.colors[i].ahundred + '"><span>A100</span><span style="float: right">' + data.colors[i].ahundred + '</span></div><div class="color" style="background-color:' + data.colors[i].atwohundred + '"><span>A200</span><span style="float: right">' + data.colors[i].atwohundred + '</span></div><div class="color" style="background-color:' + data.colors[i].afourhundred + '"><span>A400</span><span style="float: right">' + data.colors[i].afourhundred + '</span></div><div class="color" style="background-color:' + data.colors[i].asevenhundred + '"><span>A700</span><span style="float: right">' + data.colors[i].asevenhundred + '</span></div>');
-            // $('ul#grid-wrapper').append("<li class='card " + data.apps[i].appType + "'> <div class = 'app-img-wrapper'> <img style = 'background:" + data.apps[i].imgcolor + "' class ='app-img' src = '" + data.apps[i].imgsrc + "'> </img> </div> <div class = 'title-bar'> <a href = '#' class = 'card-title'>" + data.apps[i].title + " </a> <a href = '" + data.apps[i].link + "'target = '_blank' rel = 'noopener'> <img class = 'open-btn'src = 'assets/open.svg'alt = 'open-svg'/> </a> </div> </li>");
-            console.log(data.colors[i].color);
+            $('div.content').append('<div class="color-title" id="' + data.colors[i].color + '">' + data.colors[i].color + '</div><div class="color-vessel"> <div class="color" style="background-color:' + data.colors[i].fifty + '"><span>50</span><span style="float: right">' + data.colors[i].fifty + '</span></div><div class="color" style="background-color:' + data.colors[i].hundred + '"><span>100</span><span style="float: right">' + data.colors[i].hundred + '</span></div><div class="color" style="background-color:' + data.colors[i].twohundred + '"><span>200</span><span style="float: right">' + data.colors[i].twohundred + '</span></div><div class="color" style="background-color:' + data.colors[i].threehundred + '"><span>300</span><span style="float: right">' + data.colors[i].threehundred + '</span></div><div class="color" style="background-color:' + data.colors[i].fourhundred + '"><span>400</span><span style="float: right">' + data.colors[i].fourhundred + '</span></div><div class="color" style="background-color:' + data.colors[i].fivehundred + '"><span>500</span><span style="float: right">' + data.colors[i].fivehundred + '</span></div><div class="color" style="background-color:' + data.colors[i].sixhundred + '"><span>600</span><span style="float: right">' + data.colors[i].sixhundred + '</span></div><div class="color" style="background-color:' + data.colors[i].sevenhundred + '"><span>700</span><span style="float: right">' + data.colors[i].sevenhundred + '</span></div><div class="color" style="background-color:' + data.colors[i].eighthundred + '"><span>800</span><span style="float: right">' + data.colors[i].eighthundred + '</span></div><div class="color" style="background-color:' + data.colors[i].ninehundred + '"><span>900</span><span style="float: right">' + data.colors[i].ninehundred + '</span></div></div><div class="color-title" id="' + data.colors[i].color + '">' + data.colors[i].color + ' Accents</div><div class="color-vessel"><div class="color" style="background-color:' + data.colors[i].ahundred + '"><span>A100</span><span style="float: right">' + data.colors[i].ahundred + '</span></div><div class="color" style="background-color:' + data.colors[i].atwohundred + '"><span>A200</span><span style="float: right">' + data.colors[i].atwohundred + '</span></div><div class="color" style="background-color:' + data.colors[i].afourhundred + '"><span>A400</span><span style="float: right">' + data.colors[i].afourhundred + '</span></div><div class="color" style="background-color:' + data.colors[i].asevenhundred + '"><span>A700</span><span style="float: right">' + data.colors[i].asevenhundred + '</span></div>');
         });
     }
 });
+
+
+
+
+$(window).on('scroll load', function() {
+    var content = $(".content")
+    var winScroll = $(content).scrollTop();
+    // var winScroll = content.scrollTop || document.documentElement.scrollTop;
+    var height = content.scrollHeight - content.clientHeight;
+    var scrolled = (winScroll / height) * 100;
+    // document.getElementById("myBar").style.height = scrolled + "%";
+    console.log(winScroll);
+    console.log(scrolled);
+});
+
+
+// function scrollR() {
+//     document.getElementById("tags").scrollBy(80, 0);
+// }
+//
+// function scrollL() {
+//     document.getElementById("tags").scrollBy(-80, 0);
+// }
