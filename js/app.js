666e30c15ea6104eba6eb2b4f0ec8bbc0012838e
Rohit Parida <rohitkparida@gmail.com>
2019-06-12 19:30:44 +0530

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -94,53 +94,47 @@ $.ajax({
 // else
 //   console.log("#ffffff")
 
-$(document).ready(function () {
+function rgb2hex(rgb) {
+  rgb = rgb.match(/^rgb\((\d+),\s*(\d+),\s*(\d+)\)$/);
+    function hex(x) {
+      return ("0" + parseInt(x).toString(16)).slice(-2);
+    }
+  return "#" + hex(rgb[1]) + hex(rgb[2]) + hex(rgb[3]);
+}
+function rgb(hex) {
+    var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+    return result ? {
+      r: parseInt(result[1], 16),
+      g: parseInt(result[2], 16),
+      b: parseInt(result[3], 16)
+    } : null;
+}
   $(document).on("click", ".color", function () {
-    function rgb2hex(rgb) {
-      rgb = rgb.match(/^rgb\((\d+),\s*(\d+),\s*(\d+)\)$/);
-
-      function hex(x) {
-        return ("0" + parseInt(x).toString(16)).slice(-2);
-      }
-
-      return "#" + hex(rgb[1]) + hex(rgb[2]) + hex(rgb[3]);
-    }
-
-    function rgb(hex) {
-      var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
-      return result ? {
-        r: parseInt(result[1], 16),
-        g: parseInt(result[2], 16),
-        b: parseInt(result[3], 16)
-      } : null;
-    }
-
+    $(".color").each(function(){
     var txtcolor = $(this).css("background-color");
+    console.log($(this).css("background-color"))
     // $(this).css("color", txtcolor);
     var hexcolor = rgb2hex(txtcolor);
     console.log(hexcolor);
+    $(".appbar span").css("visibility", "visible");
+    setTimeout(function () {
+      $(".appbar span").css("visibility", "hidden");
+    }, 380);
 
+
+    if (rgb(hexcolor).r * 0.299 + rgb(hexcolor).g * 0.587 + rgb(hexcolor).b * 0.114 > 128)
+      $(this).css("color", "rgba(0,0,0,.71)");
+    else
+      $(this).css("color", "rgba(255,255,255,.76)");
+    });
     var input_temp = document.createElement("input");
-    input_temp.value = hexcolor;
+    input_temp.value = rgb2hex($(this).css("background-color"));
     document.body.appendChild(input_temp);
     input_temp.select();
     document.execCommand("copy");
     document.body.removeChild(input_temp);
-    $(".appbar span").css("visibility", "visible");
-    setTimeout(function () {
-      $(".appbar span").css("visibility", "hidden");
-    }, 300);
-
-
-    if (rgb(hexcolor).r * 0.299 + rgb(hexcolor).g * 0.587 + rgb(hexcolor).b * 0.114 > 128)
-      $(this).css("color", "rgba(0,0,0,.87)");
-    else
-      $(this).css("color", "rgba(255,255,255,.87)");
-
   });
 
-
-});
 
 // $(document).ready(function(){
 // $("span").each(function(){
